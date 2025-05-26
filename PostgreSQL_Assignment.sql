6362ccae20e881ee4845848259f0621178c57b35
Sohel-Developer <sohel.webdevoloper@gmail.com>
2025-05-26 12:46:29 +0600

    Complete this code

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE conservation_db
+CREATE DATABASE conservation_db2
 
 --Table Create
 DROP TABLE IF EXISTS sightings, species, rangers CASCADE;
@@ -18,7 +18,7 @@ CREATE TABLE species (
     common_name VARCHAR(100) NOT NULL,
     scientific_name VARCHAR (100) NOT NULL,
     discovery_date DATE NOT NULL,
-    conservation_status VARCHAR(50) CHECK (conservation_status IN('Endangered', 'Vulnerable'))
+    conservation_status VARCHAR(50) CHECK (conservation_status IN('Endangered', 'Vulnerable', 'Historic'))
 );
 
 
@@ -43,4 +43,96 @@ INSERT INTO rangers (ranger_id, name, region) VALUES
 (2, 'Bob White', 'River Delta'),
 (3, 'Carol King', 'Mountain Range');
 
+--Species
 
+INSERT INTO  species( species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+-- Sightings
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+
+-- manual data id already 3, so do work.
+SELECT setval(pg_get_serial_sequence('rangers', 'ranger_id'), 3, true);
+
+
+-- Problem 1: Register a new ranger
+INSERT INTO rangers ( name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+--Problem 2: Count unique species ever sighted
+
+SELECT COUNT (DISTINCT species_id) AS unique_species_count
+FROM sightings
+
+
+
+-- Problem 3: Find all sightings where location includes "Pass"
+
+SELECT *
+FROM sightings
+WHERE location ILIKE '%Pass%';
+
+-- Problem 4: List each ranger’s name and total sightings
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name;
+
+
+
+
+-- Problem 5: List species that have never been sighted
+SELECT common_name
+FROM species
+WHERE species_id NOT IN (
+    SELECT DISTINCT species_id FROM sightings
+);
+
+-- Problem 6: Show the most recent 2 sightings
+SELECT sp.common_name, s.sighting_time, r.name
+FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
+
+-- Problem 7: Update species discovered before year 1800 to 'Historic'
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+-- Problem 8: Label sighting’s time of day
+SELECT sighting_id,
+    CASE
+        WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+-- Problem 9: Delete rangers who never sighted any species
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+    SELECT DISTINCT ranger_id FROM sightings
+);
+
+--Note 9 number problem google and Another Source FOr Better In This code.
+-- DELETE FROM rangers
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM sightings
+--     WHERE sightings.ranger_id = rangers.ranger_id
+-- );
+
+
+SELECT * FROM rangers;
